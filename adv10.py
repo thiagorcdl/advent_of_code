ab2944fe9122b4cbce0a23988dec9ff9198d45b4
Thiago <thiago@smartgreen.net>
2015-12-10 09:47:24 -0200

    Day 9: part 1 and 2

---
+++ adv10.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+
+part2 = len(sys.argv) > 1 and sys.argv[1] == '2'
+f = open('./input10.txt', 'r')
+input_sequence = f.readline().rstrip()
+
+
+def look(sequence):
+    i = 0
+    say = ''
+    char = 0
+    for i, c in enumerate(sequence):
+        if c != sequence[char]:
+            say += str(i - char) + sequence[char]
+            char = i
+    say += str(i - char + 1) + sequence[char]
+    return say
+
+for n in range(50 if part2 else 40):
+    input_sequence = look(input_sequence)
+print len(input_sequence)
