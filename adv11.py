1ee45a19732674d69f79460b59eb3318811fcbf7
Thiago <thiago@smartgreen.net>
2015-12-14 15:13:41 -0200

    Day 11: part 2

--- adv11.py
+++ adv11.py
@@ -19,25 +19,31 @@ for i, char in enumerate(str_pwd):
         str_pwd = "".join(map(chr, pwd))
         break
 
-while True:
-    for i, char in enumerate(reversed(pwd)):
-        j = -(i + 1)
-        # Base is 97 ('a'); uses mod 26 for the offset
-        pwd[j] = 97 + ((char - 96) % 26)
-        if pwd[j] in ['i', 'o', 'l']:
-            # Jumps to next acceptable string | fiaa -> fjaa
-            pwd[j] = incr(pwd[i])
-        if pwd[j] != 97:
-            break
-    has_seq = False
-    for i, char in enumerate(pwd):
-        if i == len(pwd) - 2:
-            break
-        if pwd[i+1] == pwd[i]+1 and pwd[i+2] == pwd[i]+2:
-            has_seq = True
-    str_pwd = "".join(map(chr, pwd))
-    n_pairs = len(set(re.findall(r'(\w)\1', str_pwd)))
-    if n_pairs > 1 and has_seq:
-        break
 
-print str_pwd
+def find_next(pwd):
+    while True:
+        for i, char in enumerate(reversed(pwd)):
+            j = -(i + 1)
+            # Base is 97 ('a'); uses mod 26 for the offset
+            pwd[j] = 97 + ((char - 96) % 26)
+            if pwd[j] in ['i', 'o', 'l']:
+                # Jumps to next acceptable string | fiaa -> fjaa
+                pwd[j] = incr(pwd[i])
+            if pwd[j] != 97:
+                break
+        has_seq = False
+        for i, char in enumerate(pwd):
+            if i == len(pwd) - 2:
+                break
+            if pwd[i+1] == pwd[i]+1 and pwd[i+2] == pwd[i]+2:
+                has_seq = True
+        str_pwd = "".join(map(chr, pwd))
+        n_pairs = len(set(re.findall(r'(\w)\1', str_pwd)))
+        if n_pairs > 1 and has_seq:
+            break
+    return str_pwd
+
+next_pwd = find_next(pwd)
+if len(sys.argv) > 1 and sys.argv[1] == '2':
+    next_pwd = find_next(map(ord, list(next_pwd)))
+print next_pwd
