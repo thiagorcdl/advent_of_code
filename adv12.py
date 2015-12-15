373c62de779ed8133b1b4eb312f5804b533e91ce
Thiago <thiago@smartgreen.net>
2015-12-15 12:22:14 -0200

    Day 12: part 2 working

--- adv12.py
+++ adv12.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python
 import sys
 import re
+import json
 
 f = open('./input12.txt', 'r')
 injson = f.readline().rstrip()
 
-if True or len(sys.argv) > 1 and sys.argv[1] == '2':
-    injson = re.sub('("\w+":\{("\w+":("\w+"|\-?\d+|\[.*\]|\{.*\}),)*"\w+":"red"(,"\w+":("\w+"|\-?\d+|\[.*\]|\{.*\}))*\})', '', injson)
+
+def search(current):
+    items = []
+    if isinstance(current, dict):
+        items = current.iteritems()
+    elif isinstance(current, list):
+        items = enumerate(current)
+    for key, item in items:
+        if isinstance(item, dict):
+            current[key] = 0 if "red" in item.itervalues() else search(item)
+        elif isinstance(item, list):
+            current[key] = search(item)
+    return current
+
+if len(sys.argv) > 1 and sys.argv[1] == '2':
+    ldjson = json.loads(injson)
+    ldjson = search(ldjson)
+    injson = json.dumps(ldjson)
+
 print sum(map(int, re.findall(r'\-?\d+', injson)))
