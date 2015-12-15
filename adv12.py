f762489d4f210c8bc8ab79a94a153e1fd1a6e849
Thiago <thiago@smartgreen.net>
2015-12-15 11:55:09 -0200

    Day 12: part 2 attempt. doesn't work

--- adv12.py
+++ adv12.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
+import sys
 import re
 
 f = open('./input12.txt', 'r')
 injson = f.readline().rstrip()
-nums = map(int, re.findall(r'\-?\d+', injson))
 
-print sum(nums)
+if True or len(sys.argv) > 1 and sys.argv[1] == '2':
+    injson = re.sub('("\w+":\{("\w+":("\w+"|\-?\d+|\[.*\]|\{.*\}),)*"\w+":"red"(,"\w+":("\w+"|\-?\d+|\[.*\]|\{.*\}))*\})', '', injson)
+print sum(map(int, re.findall(r'\-?\d+', injson)))
