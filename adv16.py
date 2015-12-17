18e431f4734547c6486c1f51ea400cc32daa0166
Thiago <thiago@smartgreen.net>
2015-12-17 10:29:52 -0200

    Day 16: part 1 and 2

---
+++ adv16.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import re
+import sys
+
+part2 = len(sys.argv) > 1 and sys.argv[1] == '2'
+f = open('./input16.txt', 'r')
+
+aunt = []
+truth = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1,
+}
+naunt = 1
+matches = 0
+aunt = 0
+
+while True:
+    line = f.readline().rstrip()
+    if not line:
+        break
+    clues = {}
+
+    for x in re.split(r'\d+: ', line)[1].split(','):
+        key, val = x.split(":")
+        key = key.lstrip().rstrip()
+        val = int(val)
+        if part2 and ((key in ['cats', 'trees'] and val <= truth[key]) or
+           (key in ['pomeranians', 'goldfish'] and val >= truth[key]) or
+           (truth[key] != val)) or truth[key] != val:
+            continue
+        clues[key] = val
+    aunt, matches = (naunt, len(clues)) if len(clues) > matches else (aunt, matches)
+    naunt += 1
+print aunt
