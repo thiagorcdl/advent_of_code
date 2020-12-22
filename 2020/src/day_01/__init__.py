486987f084fc95a37e651ec4cbca29aea10c159f
Thiago Lima <thiagorcdl@gmail.com>
2020-12-22 03:09:16 -0300

    add 2020 day 01 part 2

--- 2020/src/day_01/__init__.py
+++ 2020/src/day_01/__init__.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-import sys
-sys.path.append("..")
 from src.utils import BaseResolution
 
 
@@ -19,3 +17,20 @@ class Resolution(BaseResolution):
                 candidate = int(candidate)
                 if rest == candidate:
                     print(line * candidate)
+                    return
+
+    @staticmethod
+    def part_2(input_lines: list):
+        """Run n**3 solution for part 2."""
+        total = 2020
+        for i, line in enumerate(input_lines):
+            line = int(line)
+            rest = total - line
+            for candidate1 in input_lines[i+1:]:
+                candidate1 = int(candidate1)
+                rest2 = rest - candidate1
+                for candidate2 in input_lines[i+2:]:
+                    candidate2 = int(candidate2)
+                    if rest2 == candidate2:
+                        print(line * candidate1 * candidate2)
+                        return
