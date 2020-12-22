a197c0612a44c6e019c547229a7fad64340f3f19
Thiago Lima <thiagorcdl@gmail.com>
2020-12-22 04:09:35 -0300

    add 2020 day 04 part 1

---
+++ 2020/src/day_04/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import re
+from src.utils import BaseResolution
+
+REQUIRED = {
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+}
+
+
+class Resolution(BaseResolution):
+    """Logics for resolving day 4."""
+    day = 4
+
+    @staticmethod
+    def part_1(input_lines: list):
+        """Run solution for part 1."""
+        total = 0
+        fields = set()
+        for line in input_lines:
+            if not line.strip():
+                if fields >= REQUIRED:
+                    total += 1
+                fields = set()
+                continue
+
+            matches = re.findall(r"(byr|iyr|eyr|hgt|hcl|ecl|pid):", line)
+            fields |= set(matches)
+
+        if fields >= REQUIRED:
+            total += 1
+        print(total)
