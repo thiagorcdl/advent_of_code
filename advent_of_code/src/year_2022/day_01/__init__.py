5774841b00305fb1d92b29e9baf3a27109d5a320
Thiago Lima <thiagorcdl@gmail.com>
2022-12-01 22:06:46 -0330

    add 2022

---
+++ advent_of_code/src/year_2022/day_01/__init__.py
@@ -0,0 +1,14 @@
+from advent_of_code.src.utils import BaseResolution
+
+
+class Resolution(BaseResolution):
+    """Logics for resolving day 1."""
+    day = 1
+
+    def part_1(self):
+        """TODO"""
+        pass
+
+    def part_2(self):
+        """TODO"""
+        pass
