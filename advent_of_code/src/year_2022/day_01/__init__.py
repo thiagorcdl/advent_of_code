28f6d1fe68bf8a8dd056b61a06cd8efd23ac3091
Thiago Lima <thiagorcdl@gmail.com>
2022-12-01 23:01:19 -0330

    add 2022 1 2

--- advent_of_code/src/year_2022/day_01/__init__.py
+++ advent_of_code/src/year_2022/day_01/__init__.py
@@ -6,7 +6,9 @@ class Resolution(BaseResolution):
     day = 1
 
     def part_1(self):
-        """Print the highest amount of calories carried by an elf."""
+        """Find the Elf carrying the most Calories.
+        How many total Calories is that Elf carrying?
+        """
         highest = current = 0
         for cal_item in self.input_lines:
             if cal_item == "":
@@ -22,5 +24,27 @@ class Resolution(BaseResolution):
         return highest
 
     def part_2(self):
-        """TODO"""
-        pass
+        """Find the top three Elves carrying the most Calories.
+        How many Calories are those Elves carrying in total?
+        """
+
+        def update_highest(highest, current):
+            for i in range(3):
+                if current > highest[i]:
+                    highest.insert(i, current)
+                    highest.pop(3)
+                    break
+
+            return highest
+
+        highest = [0, 0, 0]
+        current = 0
+        for cal_item in self.input_lines:
+            if cal_item == "":
+                highest = update_highest(highest, current)
+                current = 0
+            else:
+                current += int(cal_item)
+
+        highest = update_highest(highest, current)
+        return sum(highest)
