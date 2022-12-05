2e3c3c9eba5b9167b8db4cb4d347a8206685438e
Thiago Lima <thiagorcdl@gmail.com>
2022-12-05 13:38:21 -0330

    add 2022 5 2

--- advent_of_code/src/year_2022/day_05/__init__.py
+++ advent_of_code/src/year_2022/day_05/__init__.py
@@ -14,16 +14,27 @@ class Solution(BaseSolution):
         """Return 0-base index of a stack based on index in the string."""
         return (index - 1) // 4
 
-    def part_1(self):
-        """Find what crate ends up on top of each stack."""
-        # Build stacks
+    def build_stacks(self):
+        """Read the first lines that tell the initial crate arrangement."""
         for line in self.input_lines[:self.CMD_LINE_START - 1]:
             for match in re.finditer(r'[A-Z]', line):
                 stack_id = self.get_stack(match.start())
                 crate = match.group()
                 self.stacks[stack_id].appendleft(crate)
 
-        # Move crates
+    def get_result(self):
+        """Cocnatenate the top crates."""
+        result = ""
+        for stack in self.stacks:
+            result += stack.pop()
+        return result
+
+    def part_1(self):
+        """Find what crate ends up on top of each stack considering crates can only
+        be lifted one by one.
+        """
+        self.build_stacks()
+
         for line in self.input_lines[self.CMD_LINE_START:]:
             amount, source, target = re.findall(r'(\d+) from (\d) to (\d)', line)[0]
             amount, source, target = int(amount), int(source) - 1, int(target) - 1
@@ -32,15 +43,24 @@ class Solution(BaseSolution):
                 crate = self.stacks[source].pop()
                 self.stacks[target].append(crate)
 
-        # Get top crates
-        result = ""
-        for stack in self.stacks:
-            result += stack.pop()
-        return result
+        return self.get_result()
 
     def part_2(self):
-        """Run solution for part 2."""
-        total = 0
-        for line in self.input_lines:
-            pass
-        return total
+        """Find what crate ends up on top of each stack considering multipel crates
+        can be lifted at once.
+        """
+        self.build_stacks()
+
+        for line in self.input_lines[self.CMD_LINE_START:]:
+            amount, source, target = re.findall(r'(\d+) from (\d) to (\d)', line)[0]
+            amount, source, target = int(amount), int(source) - 1, int(target) - 1
+            fork = deque()
+            while amount:
+                amount -= 1
+                crate = self.stacks[source].pop()
+                fork.appendleft(crate)
+
+            for crate in fork:
+                self.stacks[target].append(crate)
+
+        return self.get_result()
