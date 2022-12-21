e953719f4842c5c5ef266bc3646615b7d137867c
Thiago Lima <thiagorcdl@gmail.com>
2022-12-21 02:30:51 -0330

    add 2022 21

--- advent_of_code/src/year_2022/day_21/__init__.py
+++ advent_of_code/src/year_2022/day_21/__init__.py
@@ -9,7 +9,10 @@ OPERATIONS = {
     "+": lambda x, y: x + y,
     "/": lambda x, y: x / y,
     "-": lambda x, y: x - y,
+    "=": lambda x, y: x == y,
 }
+ME = "humn"
+ROOT = "root"
 
 
 class Solution(BaseSolution):
@@ -36,14 +39,53 @@ class Solution(BaseSolution):
                 self.monkeys[match.group(1)] = {
                     "val": None,
                     "operands": [match.group(2), match.group(4)],
-                    "operator": match.group(3),
                     "operation": OPERATIONS[match.group(3)],
                 }
-        return int(self.call_monkey("root"))
+        return int(self.call_monkey(ROOT))
+
+    def repl_expr(self, expr: str):
+        replaced = True
+        while replaced:
+            replaced = False
+            for monkey in self.monkeys.values():
+                regex = monkey["regex"]
+                expr, n_replaced = regex.subn(monkey["sub"], expr)
+                if n_replaced:
+                    replaced = True
+        return expr
 
     def part_2(self):
-        """Run solution for part 2."""
-        total = 0
+        """Yield equation to be simplified.
+
+        Used https://www.mathpapa.com/simplify-calculator/
+        Got x = 889638042320048128/265335
+        Got x = 3352886133830.999
+        Rounded up to 3352886133831
+        """
         for line in self.input_lines:
-            pass
-        return total
+            if match := re.match(r"root: (\w+) ([*+/-]) (\w+)", line):
+                monkey_name = ROOT
+                self.monkeys[monkey_name] = {
+                    "regex": re.compile(monkey_name),
+                    "sub": f"({match.group(1)} == {match.group(3)})",
+                }
+            elif re.match(r"humn: (.+)", line):
+                monkey_name = ME
+                self.monkeys[monkey_name] = {
+                    "regex": re.compile(monkey_name),
+                    "sub": f"x",
+                }
+            elif match := re.match(r"(\w+): (\d+)", line):
+                monkey_name = match.group(1)
+                self.monkeys[monkey_name] = {
+                    "regex": re.compile(monkey_name),
+                    "sub": match.group(2),
+                }
+            elif match := re.match(r"(\w+): (.+)", line):
+                monkey_name = match.group(1)
+                self.monkeys[monkey_name] = {
+                    "regex": re.compile(monkey_name),
+                    "sub": f"({match.group(2)})",
+                }
+        expr = self.monkeys[ROOT]["sub"]
+        return self.repl_expr(expr)
