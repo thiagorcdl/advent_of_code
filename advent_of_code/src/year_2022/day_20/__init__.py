fba6ad8b70d4774c5c5b703191081f8ed3a38e17
Thiago Lima <thiagorcdl@gmail.com>
2022-12-20 23:43:01 -0330

    add 2022 20 2

--- advent_of_code/src/year_2022/day_20/__init__.py
+++ advent_of_code/src/year_2022/day_20/__init__.py
@@ -4,47 +4,39 @@ from advent_of_code.src.utils import BaseSolution
 class Solution(BaseSolution):
     """Logics for solving day 0."""
     day = 0
+    DEC_KEY = 811589153
 
     # example = True
 
-    def part_1(self):
-        """Run solution for part 1."""
+    def solve(self, decription_key=1, iterations=1):
+        order = []
         mixed = []
         zero_obj = None
-        for line in self.input_lines:
-            obj = {"num": int(line), "seen": False}
+        for idx, line in enumerate(self.input_lines):
+            obj = {"num": int(line) * decription_key, "id": idx}
             if obj["num"] == 0:
                 zero_obj = obj
+            order.append(obj)
             mixed.append(obj)
 
         length = len(mixed)
-        i = 0
-        count = 0
-        while count < length:
-            while mixed[i]["seen"]:
-                i = (i + 1) % length
-            obj = mixed.pop(i)
-            obj["seen"] = True
-            num = obj["num"]
-            j = (i + num) % (length - 1)
-            mixed.insert(j, obj)
-            # print(f"{num} moves from {i} to {j}")
-            if j <= i:
-                i = (i + 1) % length
-            count += 1
-            # print([x["num"] for x in mixed])
-            # print()
+        for turn in range(iterations):
+            for obj in order:
+                i = mixed.index(obj)
+                obj = mixed.pop(i)
+                j = (i + obj["num"]) % (length - 1)
+                mixed.insert(j, obj)
 
-        # print([x["num"] for x in mixed])
         zero_idx = mixed.index(zero_obj)
         return sum(
             mixed[(zero_idx + delta) % length]["num"]
             for delta in (1000, 2000, 3000)
         )
 
+    def part_1(self):
+        """Run solution for part 1."""
+        return self.solve()
+
     def part_2(self):
         """Run solution for part 2."""
-        total = 0
-        for line in self.input_lines:
-            pass
-        return total
+        return self.solve(811589153, 10)
