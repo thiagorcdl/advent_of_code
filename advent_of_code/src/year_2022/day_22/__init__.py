f2d631ee54c2ee8f052f43bbae552c0a792530e0
Thiago Lima <thiagorcdl@gmail.com>
2022-12-22 04:42:27 -0330

    add 2022 22 2 WIP

--- advent_of_code/src/year_2022/day_22/__init__.py
+++ advent_of_code/src/year_2022/day_22/__init__.py
@@ -25,6 +25,69 @@ ROT_MODIF = {
     "L": -1,
 }
 
+EDGES = {
+    "A1": [(0, 50), (0, 99)],
+    "A2": [(150, 0), (199, 0)],
+    "B1": [(0, 50), (49, 50)],
+    "B2": [(100, 0), (149, 0)],
+    "C1": [(0, 100), (0, 149)],
+    "C2": [(199, 0), (199, 49)],
+    "D1": [(0, 149), (49, 149)],
+    "D2": [(100, 99), (149, 99)],
+    "E1": [(49, 100), (49, 149)],
+    "E2": [(50, 99), (99, 99)],
+    "F1": [(50, 50), (99, 50)],
+    "F2": [(100, 0), (100, 49)],
+    "G1": [(149, 50), (149, 99)],
+    "G2": [(150, 49), (199, 49)],
+}
+for k, v in EDGES.items():
+    EDGES[k] = [(x[0] + 1, x[1] + 1) for x in v]
+
+HORIZONTAL = 0
+VERTICAL = 1
+
+EDGE_ORIENTATION = {
+    "A1": HORIZONTAL,
+    "A2": VERTICAL,
+    "B1": VERTICAL,
+    "B2": VERTICAL,
+    "C1": HORIZONTAL,
+    "C2": HORIZONTAL,
+    "D1": VERTICAL,
+    "D2": VERTICAL,
+    "E1": HORIZONTAL,
+    "E2": VERTICAL,
+    "F1": VERTICAL,
+    "F2": HORIZONTAL,
+    "G1": HORIZONTAL,
+    "G2": VERTICAL,
+}
+
+JUMP_MAP = {
+    "A1": ("A2", RIGHT),
+    "A2": ("A1", DOWN),
+    "B1": ("B2", RIGHT),
+    "B2": ("B1", RIGHT),
+    "C1": ("C2", UP),
+    "C2": ("C1", DOWN),
+    "D1": ("D2", LEFT),
+    "D2": ("D1", LEFT),
+    "E1": ("E2", LEFT),
+    "E2": ("E1", UP),
+    "F1": ("F2", DOWN),
+    "F2": ("F1", RIGHT),
+    "G1": ("G2", LEFT),
+    "G2": ("G1", UP),
+}
+
+JUMPS_DIRS = {
+    RIGHT: ["D1", "E2", "D2", "G2"],
+    DOWN: ["E1", "G1", "C2"],
+    LEFT: ["B1", "F1", "B2", "A2"],
+    UP: ["A1", "C1", "F2"],
+}
+
 
 class Solution(BaseSolution):
     """Logics for solving day 22."""
@@ -92,7 +155,7 @@ class Solution(BaseSolution):
         next_val = self.board[next_pos[0]][next_pos[1]]
         return next_pos, next_val
 
-    def follow_directions(self):
+    def follow_directions(self, part2=False):
         """Simulate walking through the board."""
         while self.instructions:
             inst = self.instructions.pop(0)
@@ -102,7 +165,9 @@ class Solution(BaseSolution):
 
             n_moves = int(inst)
             while n_moves:
-                next_pos, next_val = self.get_next_cell()
+                next_pos, next_val = (
+                    self.get_next_cell_cube() if part2 else self.get_next_cell()
+                )
                 if next_val == PATH:
                     # Keep moving
                     self.pos = next_pos
@@ -121,9 +186,60 @@ class Solution(BaseSolution):
         self.follow_directions()
         return self.calculate_result()
 
+    def get_edge_list(self, edge_id):
+        edge_start, edge_end = EDGES[edge_id]
+        start_l, start_c = edge_start
+        end_l, end_c = edge_end
+        orientation = EDGE_ORIENTATION[edge_id]
+        if orientation == HORIZONTAL:
+            # Fixed line, variable column
+            return [x for x in range(start_c, end_c+1)]
+        else:
+            # Fixed column, variable line
+            return [x for x in range(start_l, end_l+1)]
+
+    def get_next_cell_cube(self):
+        """Calculate next pos and get value around the cube."""
+        modif = DIR_MODIFS[self.facing]
+        next_pos = [self.pos[i] + modif[i] for i in range(2)]
+        next_val = self.board[next_pos[0]][next_pos[1]]
+        if next_val != VOID:
+            return next_pos, next_val
+
+        for edge_id in JUMPS_DIRS[self.facing]:
+            edge_start, edge_end = EDGES[edge_id]
+            start_l, start_c = edge_start
+            end_l, end_c = edge_end
+            if not (start_l <= self.pos[0] <= end_l and start_c <= self.pos[1] <= end_c):
+                continue
+
+            # Make jump
+            next_edge_id, next_facing = JUMP_MAP[edge_id]
+            # print(f"Jumping from {edge_id} to {next_edge_id}")
+            next_edge_start, next_edge_end = EDGES[next_edge_id]
+            next_start_l, next_start_c = next_edge_start
+            curr_edge_list = self.get_edge_list(edge_id)
+
+            if EDGE_ORIENTATION[edge_id] == HORIZONTAL:
+                idx_in_edge = curr_edge_list.index(self.pos[1])
+            else:
+                idx_in_edge = curr_edge_list.index(self.pos[0])
+
+            if EDGE_ORIENTATION[next_edge_id] == HORIZONTAL:
+                # Map vertical to horizontal
+                next_pos = next_start_l, next_start_c + idx_in_edge
+            else:
+                # Same orientation, same coord mapping
+                next_pos = next_start_l + idx_in_edge, next_start_c
+
+            self.facing = next_facing
+            break
+
+        next_val = self.board[next_pos[0]][next_pos[1]]
+        return next_pos, next_val
+
     def part_2(self):
         """Run solution for part 2."""
-        total = 0
-        for line in self.input_lines:
-            pass
-        return total
+        self.read_input()
+        self.follow_directions(part2=True)
+        return self.calculate_result()  # 140214 < ANSWER
