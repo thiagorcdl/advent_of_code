e476b66c9ec97ce76732332ab28d8e22cdf25055
Thiago Lima <thiagorcdl@gmail.com>
2024-01-04 00:36:40 -0500

    add 2023 02 1

--- advent_of_code/src/_version.py
+++ advent_of_code/src/_version.py
@@ -1,6 +1,6 @@
 MAJOR_VERSION = 2
-MINOR_VERSION = 25
-PATCH_VERSION = 0
+MINOR_VERSION = 26
+PATCH_VERSION = 1
 
 __version_info__ = (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 __version__ = ".".join([str(info) for info in __version_info__])
--- advent_of_code/src/year_2023/day_02/__init__.py
+++ advent_of_code/src/year_2023/day_02/__init__.py
@@ -1,13 +1,20 @@
 from advent_of_code.src.utils import BaseSolution
 import re
 
+
+REGEXES = {
+    "red": re.compile(r"(\d+) red"),
+    "green": re.compile(r"(\d+) green"),
+    "blue": re.compile(r"(\d+) blue"),
+}
+
+
 class Solution(BaseSolution):
     """Logics for solving day 2."""
     day = 2
     games = []
 
-    # example = True
-    # > 1916
+    example = True
 
     def part_1(self):
         """Determine which games would have been possible if the bag had been loaded with
@@ -19,15 +26,10 @@ class Solution(BaseSolution):
             "green": 13,
             "blue": 14,
         }
-        res = {
-            "red": re.compile(r"(\d+) red"),
-            "green": re.compile(r"(\d+) green"),
-            "blue": re.compile(r"(\d+) blue"),
-        }
         total = 0
         for idx, line in enumerate(self.input_lines):
-            for color in minimums:
-                matches = res[color].findall(line)
+            for color, regex in REGEXES.items():
+                matches = regex.findall(line)
                 maximum = max([int(x) for x in matches])
                 if maximum > minimums[color]:
                     break
@@ -37,10 +39,17 @@ class Solution(BaseSolution):
         return total
 
     def part_2(self):
-        """Run solution for part 2."""
+        """For each game, find the minimum set of cubes that must have been present.
+        What is the sum of the power of these sets?
+        """
         total = 0
+        maximums = {k: 0 for k in REGEXES}
         for line in self.input_lines:
-            pass
+            power = 1
+            for color, regex in REGEXES.items():
+                matches = regex.findall(line)
+                power *= max([int(x) for x in matches])
+            total += power
         return total
 
 
