a2ac45f2b474879905afc1c2ca6b897d929233fd
Thiago Lima <thiagorcdl@gmail.com>
2024-01-05 20:22:44 -0500

    add 2023 05 2

--- advent_of_code/src/year_2023/day_05/__init__.py
+++ advent_of_code/src/year_2023/day_05/__init__.py
@@ -7,7 +7,6 @@ class Solution(BaseSolution):
     day = 5
 
     # example = True
-    # < 861012996
 
     def part_1(self):
         """Return the lowest location number that corresponds to any of the
@@ -46,18 +45,14 @@ class Solution(BaseSolution):
             for x in re.findall(r"(\d+) (\d+)", self.input_lines[0])
         }
 
-        print(f"{targets=}")
         sources = set()
         for line in self.input_lines[2:]:
-            print(f"\n----------------------------------")
-            print(f"{line=}")
             match = re.match(r"(\d+) (\d+) (\d+)", line)
             if not match:
                 if ":" in line:
                     # New section
                     sources |= targets
                     targets = set()
-                    print(f"{sources=}")
                 continue
 
             target_map_start = int(match.group(1))
@@ -71,15 +66,14 @@ class Solution(BaseSolution):
                 target_map_start, map_range, targets
             )
             while remainders:
-                print("REMAINDERS")
                 remainders, sources2, targets = self.apply_mappings(
                     remainders, source_map_start, source_map_end,
                     target_map_start, map_range, targets
                 )
                 sources |= sources2
-                print(f"Merged source: {sources=}")
 
-        return min([t[0] for t in sources])
+        targets |= sources
+        return min([t[0] for t in targets])
 
     def apply_mappings(
         self, sources, source_map_start, source_map_end,
@@ -89,7 +83,6 @@ class Solution(BaseSolution):
 
         # Apply mappings
         for seed_group in [x for x in sources]:
-            print(f"{seed_group=}")
             seed_group_start = seed_group[0]
             seed_group_range = seed_group[1]
             seed_group_end = seed_group_start + seed_group_range
@@ -98,7 +91,6 @@ class Solution(BaseSolution):
                 source_map_start <= seed_group_start
                 and source_map_end >= seed_group_end
             ):
-                print("\t1")
                 # 1 - Source being mapped contains seed range
                 result = (
                     target_map_start + seed_offset,
@@ -108,7 +100,6 @@ class Solution(BaseSolution):
                 source_map_start <= seed_group_start
                 < source_map_end < seed_group_end
             ):
-                print("\t2")
                 # 2 - Source being mapped partially contains seed range (lower)
                 result = (
                     target_map_start + seed_offset,
@@ -120,12 +111,10 @@ class Solution(BaseSolution):
                     (seed_group_start + seed_group_range) - source_map_end,
                 )
                 remainders.add(remainder)
-                print(f"\t{remainders=}")
             elif (
                 seed_group_start < source_map_start
                 < seed_group_end <= source_map_end
             ):
-                print("\t3")
                 # 3 - Source being mapped partially contains seed range (upper)
                 result = (
                     target_map_start,
@@ -137,12 +126,10 @@ class Solution(BaseSolution):
                     source_map_start - seed_group_start,
                 )
                 remainders.add(remainder)
-                print(f"\t{remainders=}")
             elif (
                 source_map_start > seed_group_start
                 and source_map_end < seed_group_end
             ):
-                print("\t4")
                 # 4 - Source being mapped is contained seed range
                 result = (
                     target_map_start,
@@ -160,16 +147,10 @@ class Solution(BaseSolution):
                     source_map_start - seed_group_start,
                 )
                 remainders.add(remainder)
-                print(f"\t{remainders=}")
             else:
                 continue
 
-            print(f"\t >> removing {seed_group=} from sources")
             sources.remove(seed_group)
             targets.add(result)
-            print(f"\t{sources=}")
-            print(f"\t{targets=}\n-")
-        print(f"{remainders=}")
-        print(f"{sources=}")
-        print(f"{targets=}\n")
+
         return remainders, sources, targets
